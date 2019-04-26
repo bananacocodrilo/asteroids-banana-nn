2625429371a58cba75b34acaa485fd2ff581c28e
alpe <you@example.com>
2019-04-26 12:24:19 +0200

    limited the usage of tf.tidy to improve performance

--- BananaNN.js
+++ BananaNN.js
@@ -4,43 +4,41 @@ function BananaNN(genome){
     this.genome = new Array(4);
     this.xs;
 
-    tf.tidy(() => {
-        if(genome) {
-            for(var i = 0; i < genome.length; i++){
-                this.genome[i] = tf.tensor(genome[i].values, genome[i].shape);
-            }
+    if(genome) {
+        for(var i = 0; i < genome.length; i++){
+            this.genome[i] = tf.tensor(genome[i].values, genome[i].shape);
         }
-        
-        this.model = tf.sequential();
-        this.hiddenLayer = tf.layers.dense({
-            inputShape: [10],
-            units: 12,
-            activation: 'sigmoid'
-        });
-        
-        this.outputLayer = tf.layers.dense({
-            units: 4,
-            activation: 'sigmoid'
-        });
-        
-        
-        this.model.add(this.hiddenLayer);
-        this.model.add(this.outputLayer);
-        if(genome){
-            this.model.setWeights(this.genome);
-        }
+    }
+    
+    this.model = tf.sequential();
+    this.hiddenLayer = tf.layers.dense({
+        inputShape: [10],
+        units: 12,
+        activation: 'sigmoid'
     });
+    
+    this.outputLayer = tf.layers.dense({
+        units: 4,
+        activation: 'sigmoid'
+    });
+    
+    
+    this.model.add(this.hiddenLayer);
+    this.model.add(this.outputLayer);
+    if(genome){
+        this.model.setWeights(this.genome);
+    }
         
-
 
     this.compute = (inputs) => {
         tf.tidy(() => {
-            xs = tf.tensor2d([inputs])
+            xs = tf.tensor2d([inputs]);
             this.output = this.model.predict(xs).dataSync()    
-        });
-        // console.log(this.output)
-        return this.output;
 
+        })
+        // tf.dispose(this.xs);
+        
+        return this.output;        
     }
 
 
